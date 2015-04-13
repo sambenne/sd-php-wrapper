6392cce6441fd7e159af2a878577e228ce8bd6b8
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2015-04-13 16:44:26 +0100

    Extension of testcase to provide mock for api

---
+++ test/serverdensity/Api/TestCase.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace serverdensity\Tests\Api;
+
+abstract class TestCase extends \PHPUnit_Framework_TestCase
+{
+    abstract protected function getApiClass();
+
+    protected function getApiMock()
+    {
+        $httpClient = $this->getMock('Guzzle\Http\Client', array('send'));
+        $httpClient
+            ->expects($this->any())
+            ->method('send');
+
+        $mock = $this->getMock('serverdensity\HttpClient\HttpClient', array(), array(array(), $httpClient));
+
+        $client = new \serverdensity\Client($mock);
+        $client->setHttpClient($mock);
+
+        return $this->getMockBuilder($this->getApiClass())
+            ->setMethods(array('get', 'post', 'postRaw', 'patch', 'delete', 'put'))
+            ->setConstructorArgs(array($client))
+            ->getMock();
+    }
+}
