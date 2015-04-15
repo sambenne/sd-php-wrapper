3fb14ba88277a6ee7479a4e4589f295f07ccdf1b
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2015-04-15 14:55:05 +0100

    Tests and functionality for users

--- lib/serverdensity/Api/Users.php
+++ lib/serverdensity/Api/Users.php
@@ -4,5 +4,55 @@ namespace serverdensity\Api;
 
 class Users extends AbstractApi
 {
+    /**
+    * Find user by ID
+    * @link     https://apidocs.serverdensity.com/#viewing-a-user
+    * @param    string  $id the id of the user.
+    * @return   an array that is the user.
+    */
+    public function getUser($id){
+        return $this->get('users/users/'.rawurlencode($id));
+    }
 
+    /**
+    * Get all users
+    * @link     https://apidocs.serverdensity.com/#listing-all-users
+    * @return   an array of arrays that contains the users.
+    */
+    public function all(){
+        return $this->get('users/users/');
+    }
+
+    /**
+    * Delete user
+    * @link     https://apidocs.serverdensity.com/#deleting-a-user
+    * @param    string $id the id of the user
+    * @return   an array with the user that got deleted
+    */
+    public function deleteUser($id)
+    {
+        return $this->delete('users/users/'.rawurldecode($id));
+    }
+
+    /**
+    * Create a user
+    * @link     https://apidocs.serverdensity.com/#creating-a-user
+    * @param    array $user
+    * @return   an array with the user that got created
+    */
+    public function createUser(array $user)
+    {
+        return $this->post('users/users/', $user);
+    }
+
+    /**
+    * Update a user
+    * @link     https://apidocs.serverdensity.com/#updating-a-user
+    * @param    array $fields
+    * @return   an array with the fields that got updated plus when it got updated
+    */
+    public function updateUser($fields)
+    {
+        return $this->put('users/users/', $fields);
+    }
 }
--- test/serverdensity/Tests/Api/UsersTest.php
+++ test/serverdensity/Tests/Api/UsersTest.php
@@ -6,7 +6,7 @@ class UsersTest extends TestCase
 {
     protected function getApiClass()
     {
-        return 'serverdensity\Api\User';
+        return 'serverdensity\Api\Users';
     }
 
     /**
@@ -14,14 +14,81 @@ class UsersTest extends TestCase
     */
     public function shouldGetUser()
     {
-        $expectedArray = array('id' => '1', 'username' => 'Joe');
+        $expectedArray = array('_id' => '1', 'username' => 'Joe');
 
         $api = $this->getApiMock();
         $api->expects($this->once())
             ->method('get')
-            ->with('users/1')
+            ->with('users/users/1')
             ->will($this->returnValue($expectedArray));
 
         $this->assertEquals($expectedArray, $api->getUser('1'));
+    }
+
+    /**
+    * @test
+    */
+    public function shouldGetAllUsers(){
+        $expectedArray = array(
+            array('_id' => '1', 'username' => 'Joe'),
+            array('_id' => '2', 'username' => 'Joe2')
+        );
+
+        $api = $this->getApiMock();
+        $api->expects($this->once())
+            ->method('get')
+            ->with('users/users/')
+            ->will($this->returnValue($expectedArray));
+
+        $this->assertEquals($expectedArray, $api->all());
+    }
+
+    /**
+    * @test
+    */
+    public function shouldDeleteUser(){
+        $expectedArray = array('_id' => '1');
+
+        $api = $this->getApiMock();
+        $api->expects($this->once())
+            ->method('delete')
+            ->with('users/users/1')
+            ->will($this->returnValue($expectedArray));
+
+        $this->assertEquals($expectedArray, $api->deleteUser('1'));
+    }
+
+    /**
+    * @test
+    */
+    public function shouldCreateUser(){
+        $expectedArray = array(
+            '_id' => '1',
+            'username' => 'Joe'
+        );
+
+        $api = $this->getApiMock();
+        $api->expects($this->once())
+            ->method('post')
+            ->with('users/users/')
+            ->will($this->returnValue($expectedArray));
+
+        $this->assertEquals($expectedArray, $api->createUser($expectedArray));
+    }
+
+    /**
+    * @test
+    */
+    public function shouldUpdateUser(){
+        $expectedArray = array('_id' => '1', 'username' => 'Joe');
+        $input = array('username' => 'Joe');
+
+        $api = $this->getApiMock();
+        $api->expects($this->once())
+            ->method('put')
+            ->with('users/users/')
+            ->will($this->returnValue($expectedArray));
+
+        $this->assertEquals($expectedArray, $api->updateUser($input));
     }
 }
